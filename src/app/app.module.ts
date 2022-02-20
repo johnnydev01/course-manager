00a45e988fec34e88cc51a95d22ce4d3e2f32b12
Johnny Silva <johnnysilva.dev@gmail.com>
2022-02-20 15:00:57 -0300

    criando formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CourseInfoComponent } from './course/course-info.component';
 import { CourseListComponent } from './course/couse-list.component';
 import { Error404Component } from './error-404/error-404.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -17,20 +18,25 @@ import { StarComponent } from './star/star.component';
     StarComponent,
     ReplacePipe,
     NavBarComponent,
-    Error404Component
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
         {
+          path: 'courses',
+          component: CourseListComponent
+        },
+        {
+          path: 'courses/info/:id',
+          component: CourseInfoComponent
+        },
+        {
           path: '',
           redirectTo: 'courses',
           pathMatch: 'full',
-        },
-        {
-          path: 'courses',
-          component: CourseListComponent
         },
         {
           path: '**',
