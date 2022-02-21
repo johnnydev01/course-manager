352b86db0f08d24fa49ef022b6bc9ded1692b648
Johnny Silva <johnnysilva.dev@gmail.com>
2022-02-21 12:35:56 -0300

    conhecendo a pasta shared

---
+++ src/app/shared/component/star/star.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { StarComponent } from "./star.component";
+
+
+@NgModule({
+    declarations: [
+        StarComponent
+    ],
+    imports: [],
+    exports: [
+        StarComponent
+    ]
+})
+export class StarModule {
+
+}
---
+++ src/app/shared/pipe/app-pipe.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { ReplacePipe } from "./replace.pipe";
+
+@NgModule({
+    declarations: [ReplacePipe],
+    imports: [],
+    exports: [ReplacePipe]
+})
+export class AppPipeModule {
+
+}
